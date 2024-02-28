d763b257d39991e6ebc83ddbb49406a86c6f6b9d
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-02-28 15:25:09 +0700

    fisith sesi 2

--- sesi_2/sid.go
+++ sesi_2/sid.go
@@ -14,9 +14,28 @@ func main() {
 	}
 }
 */
-
+/*
 func main() {
 	var city []byte = []byte{74, 97, 107, 97, 114, 116, 97}
 
 	fmt.Println(string(city))
+}
+*/
+
+// loop over string rune by rune
+/*
+func main() {
+	str1 := "makan"
+	str2 := "manca"
+
+	fmt.Printf("str1 byte lenght => %d\n", len(str1))
+	fmt.Printf("str2 byte lenght => %d\n", len(str2))
+}
+*/
+func main() {
+	str := "manca"
+
+	for i, s := range str {
+		fmt.Printf("index =>%d, dtring => %s\n", i, string(s))
+	}
 }
