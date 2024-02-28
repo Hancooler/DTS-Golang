1b95a9ff433d57bd30f74b40cf2393b7cb6948d1
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-02-28 12:13:09 +0700

    overstirng

--- sesi_2/map.go
+++ sesi_2/map.go
@@ -15,6 +15,8 @@ func main() {
 	fmt.Println("address:", person["address"])
 }
 */
+
+/* map with looping
 func main() {
 	var person = map[string]string{
 		"name":    "aril",
@@ -24,6 +26,66 @@ func main() {
 
 	for key, value := range person {
 		fmt.Println(key, ":", value)
+	}
+
+}
+*/
+
+/* map deleting value
+func main() {
+	var person = map[string]string{
+		"name":    "aril",
+		"age":     "23",
+		"address": "sudirman",
+	}
+
+	fmt.Println("before deleting:", person)
+
+	delete(person, "age")
+
+	fmt.Println("after deleting", person)
+
+}
+*/
+
+/* map detecting a value
+func main() {
+	var person = map[string]string{
+		"name":    "aril",
+		"age":     "23",
+		"address": "sudirman",
+	}
+
+	value, exist := person["age"]
+
+	if exist {
+		fmt.Println(value)
+	} else {
+		fmt.Println("value doesn'nt exist")
+	}
+
+	delete(person, "age")
+
+	value, exist = person["age"]
+
+	if exist {
+		fmt.Println(value)
+	} else {
+		fmt.Println("value has been deleted")
+	}
+
+}
+*/
+
+func main() {
+	var person = map[string]string{
+		{"name": "aril", "age", "23"},
+		{"name": "nanda", "age", "23"},
+		{"name": "mail", "age", "23"},
+	}
+
+	for i, person := range person {
+		fmt.Printf("index: %d, name: %s, age: %s\n", i, person["name"], person["age"])
 	}
 
 }
---
+++ sesi_2/sid.go
@@ -0,0 +1,22 @@
+// string in depth
+package main
+
+import (
+	"fmt"
+)
+
+/* looping over string (byte by byte)
+func main() {
+	city := "jakarta"
+
+	for i := 0; i < len(city); i++ {
+		fmt.Printf("index: %d, byte: %d\n", i, city[i])
+	}
+}
+*/
+
+func main() {
+	var city []byte = []byte{74, 97, 107, 97, 114, 116, 97}
+
+	fmt.Println(string(city))
+}
