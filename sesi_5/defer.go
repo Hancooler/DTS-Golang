ba27b3acb295679e0f9e80610f11d2b27549bd26
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-03-04 10:57:35 +0700

    Update defer.go

--- sesi_5/defer.go
+++ sesi_5/defer.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+/* defer
 func main() {
 	defer fmt.Println("world")
 	fmt.Println("hello")
+}
+*/
+/*
+func main() {
+	fmt.Println("hello")
+	defer fmt.Println("world")
+}
+
+func calldeferfunc() {
+	defer deferFunc()
+}
+
+func deferFunc() {
+	fmt.Println("defer startS")
+}
+*/
+
+func main() {
+	fmt.Println("hello")
+	defer fmt.Println("world")
+	os.Exit(1)
 }
