e7efb144be8efee48837bc255822cd963b36398a
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-03-04 10:52:10 +0700

    sesi 5

--- sesi_5/channel.go
+++ sesi_5/channel.go
@@ -74,3 +74,72 @@ func introduce(student string, c chan string) {
 	c <- result
 }
 */
+/*channelwith unbuffer
+func main() {
+	c1 := make(chan int)
+
+	go func(c chan int) {
+		fmt.Println("goroutine start sending data ")
+		c <- 10
+		fmt.Println("goroutine finish sending data ")
+	}(c1)
+
+	fmt.Println("goroutine sleep 2 second")
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("main receive data")
+	d := <-c1
+	fmt.Println("main finish receive data", d)
+
+	close(c1)
+	time.Sleep(time.Second)
+}
+*/
+/* goroutine with buffer
+func main() {
+	c1 := make(chan int)
+
+	go func(c chan int) {
+		for i := 1; i < 5; i++ {
+			fmt.Println("goroutine start sending data\n ", i)
+			c <- i
+			fmt.Println("goroutine finish sending data\n ", i)
+
+		}
+		close(c)
+	}(c1)
+
+	fmt.Println("goroutine sleep 2 second")
+	time.Sleep(2 * time.Second)
+
+	for v := range c1 {
+		fmt.Println("main receive data\n ", v)
+	}
+}
+*/
+/* channel with select
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "hello"
+	}()
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c2 <- "world"
+
+	}()
+
+	for i := 1; i < +2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+}
+*/
