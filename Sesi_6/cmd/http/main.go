e2c17083fe4db061ee19882f351d858196a75387
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-03-08 19:56:50 +0700

    fix api

--- Sesi_6/cmd/http/main.go
+++ Sesi_6/cmd/http/main.go
@@ -1,19 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-var PORT = ":5000"
-
 func main() {
-	http.HandleFunc("/", greet)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
 
-	http.ListenAndServe(PORT, nil)
+	})
+
+	fmt.Println("server is runnung on port 5000")
+
+	http.HandleFunc("/employee", getEmployee)
+	http.HandleFunc("/employee/create", createEmployee)
+
+	http.ListenAndServe(":5000", nil)
 }
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	msg := "Hello World!"
-	fmt.Fprintf(w, msg)
+type Employee struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Age      int    `json:"age"`
+	Division string `json:"division"`
+}
+
+var employees = []Employee{
+	{ID: 1, Name: "aril", Age: 23, Division: "curriculum developer"},
+	{ID: 2, Name: "nanda", Age: 23, Division: "finance"},
+	{ID: 3, Name: "mail", Age: 23, Division: "marketing"},
+}
+
+func getEmployee(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "GET" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	// set header to return json
+	w.Header().Set("Content-Type", "application/json")
+	// set Status to 200 (ok)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(employees)
+}
+
+func createEmployee(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// set header to return json
+	w.Header().Set("Content-Type", "application/json")
+	// set Status to 201 (created)
+	w.WriteHeader(http.StatusCreated)
+
+	var employee Employee
+	json.NewDecoder(r.Body).Decode(&employee)
+	employees = append(employees, employee)
+	json.NewEncoder(w).Encode(employee)
+
 }
