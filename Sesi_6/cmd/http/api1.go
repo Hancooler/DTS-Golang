c4cdc2b31c1823de59a9bdb82ca589e12760944f
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-03-08 16:08:04 +0700

    set json

--- Sesi_6/cmd/http/api1.go
+++ Sesi_6/cmd/http/api1.go
@@ -4,17 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type Employee struct {
-	ID       int
-	Name     string
-	Age      int
-	Division string
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Age      int    `json:"age"`
+	Division string `json:"division"`
 }
 
 var employees = []Employee{
-
 	{ID: 1, Name: "aril", Age: 23, Division: "curriculum developer"},
 	{ID: 2, Name: "nanda", Age: 23, Division: "finance"},
 	{ID: 3, Name: "mail", Age: 23, Division: "marketing"},
@@ -24,6 +24,8 @@ var PORT = ":5000"
 
 func main() {
 	http.HandleFunc("/employees", getEmployees)
+	http.HandleFunc("/employee", createEmployee) // Ini diperbaiki dari http.Handle ke http.HandleFunc
+
 	fmt.Println("Application is listening on port", PORT)
 	http.ListenAndServe(PORT, nil)
 }
@@ -35,5 +37,42 @@ func getEmployees(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(employees)
 		return
 	}
+
+	http.Error(w, "Invalid request method", http.StatusBadRequest)
+}
+
+func createEmployee(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "POST" {
+		err := r.ParseForm() // Tambahkan baris ini untuk parse form data
+		if err != nil {
+			http.Error(w, "Error parsing the form", http.StatusBadRequest)
+			return
+		}
+
+		name := r.FormValue("name")
+		age := r.FormValue("age")
+		division := r.FormValue("division")
+
+		convertAge, err := strconv.Atoi(age)
+		if err != nil {
+			http.Error(w, "Invalid age", http.StatusBadRequest)
+			return
+		}
+
+		newEmployee := Employee{
+			ID:       len(employees) + 1,
+			Name:     name,
+			Age:      convertAge,
+			Division: division,
+		}
+
+		employees = append(employees, newEmployee)
+
+		json.NewEncoder(w).Encode(newEmployee)
+		return
+	}
+
 	http.Error(w, "Invalid request method", http.StatusBadRequest)
 }
---
+++ Sesi_6/cmd/http/api2.go
@@ -0,0 +1,22 @@
+{
+	{
+		"ID"; 1,
+		"Name"; "aril",
+		"Age"; 23,
+		"Division"; "curriculum developer"
+	},
+
+	{
+		"ID"; 2,
+		"Name"; "nanda",
+		"Age"; 23,
+		"Division"; "finance"
+	},
+	{
+		"ID"; 3,
+		"Name"; "mail",
+		"Age"; 23,
+		"Division"; "marketing"
+	}
+}
+
