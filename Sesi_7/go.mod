6821aa05a73cffceb693eb4c82677baeb608ea6e
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-03-12 13:17:15 +0700

    db

---
+++ Sesi_7/go.mod
@@ -0,0 +1,3 @@
+module sesi_7
+
+go 1.22.0
---
+++ Sesi_7/pkg/sql.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1"
+	dbname   = "postgres"
+)
+
+var (
+	db  *sql.DB
+	err error
+)
+
+func sqlInit() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Successfully connected!")
+}
