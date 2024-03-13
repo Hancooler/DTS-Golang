b9f739fe4a8dbb8624c1960dcd827401cfd50e85
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-03-13 14:09:27 +0700

    db

--- Assigment2/helper/db.go
+++ Assigment2/helper/db.go
@@ -1,24 +1,70 @@
-// db.go
-package main
+package helper
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	_ "github.com/lib/pq"
 )
 
-var DB *gorm.DB
+// InitializeDB initialize database
+var (
+	username = "postgres"
+	password = "1"
+	dbDriver = "postgres"
+	dbPort   = "5432"
+	dbName   = "db"
+	host     = "localhost"
+	db       *sql.DB
+	err      error
+)
 
-func ConnectDatabase() {
-	dsn := "host=localhost user=postgres dbname=orders_by password= port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+func createRequiredTables() {
+	itemsTable := `
+			CREATE TABLE IF NOT EXISTS items (
+	 			id SERIAL PRIMARY KEY,
+	 			itemCode varchar(10) NOT NULL,
+	 			description TEXT NOT NULL,
+	 			quantity int NOT NULL,
+	 			orderId int NOT NULL
+	 		);
+`
+	ordersTable := `
+		CREATE TABLE IF NOT EXISTS ordersItems (
+	  		id SERIAL PRIMARY KEY,
+	 		customerName varchar (255) NOT NULL,
+	 		orderedAt timestamptz NOT NULL DEFAULT (now())
+	 		);
+	 `
+
+	createTableQueries := fmt.Sprintf("%s ", itemsTable)
+	_, err = db.Exec(createTableQueries)
 
 	if err != nil {
-		log.Fatal("Failed to connect to database!", err)
+		log.Fatal("error while creating bukan movies table =>", err.Error())
+	}
+}
+
+func InitializeDB() {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", username, password, host, dbPort, dbName)
+
+	db, err = sql.Open(dbDriver, dsn)
+
+	if err != nil {
+		log.Fatal("error connecting to database", err.Error())
 	}
 
-	database.AutoMigrate(&Order{})
+	err = db.Ping()
 
-	DB = database
+	if err != nil {
+		log.Fatal("error while tyring to ping the database connection", err.Error())
+	}
+
+	fmt.Println("successfully connected to my database")
+	createRequiredTables()
+}
+
+func GetDB() *sql.DB {
+	return db
 }
