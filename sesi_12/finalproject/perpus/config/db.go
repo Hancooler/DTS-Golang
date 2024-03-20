907b234f9d55b38f11bae6a3a3dbea0758102131
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-03-20 22:48:01 +0700

    tes

---
+++ sesi_12/finalproject/perpus/config/db.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"perpus/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+	dsn := "host=localhost user=myuser password=mypassword dbname=mydb port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
+func main() {
+	// Establish database connection
+	db, err := ConnectDB()
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&models.Book{}, &models.Borrowing{}, &models.Member{}, &models.Petugas{})
+
+	// Use db to perform operations...
+}
--- sesi_12/finalproject/perpus/models/petugas.go
+++ sesi_12/finalproject/perpus/models/petugas.go
@@ -6,31 +6,7 @@ import (
 
 type Petugas struct {
 	gorm.Model
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Nip      string `json:"nip"`
-	Password string `json:"password"`
-	Role     uint   `json:"role"`
-}
-
-type ResponsePetugas struct {
-	ID       uint
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Nip      string `json:"nip"`
-	Role     uint   `json:"role"`
-}
-
-type RequestPetugas struct {
-	ID       uint
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Nip      string `json:"nip"`
-	Password string `json:"password"`
-	Role     uint   `json:"role"`
-}
-
-type TokenRequest struct {
-	Nip      string `json:"nip"`
-	Password string `json:"password"`
+	Name       string      `json:"name"`
+	Email      string      `json:"email"`
+	Borrowings []Borrowing `json:"borrowings"`
 }
